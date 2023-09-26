da0a12700104ab5b572a408cb34df0ebd654173f
John Doe <hamedhasan422421@gmail.com>
2023-09-26 15:30:04 +0300

    🚀 Added DashboardLayout component. 🛠️ Refactored DashboardLayout code. 🎨 Improved DashboardLayout styling. 📝 Updated comments in DashboardLayout.

---
+++ src/app/(withlayout)/course/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CoursesPage = () => {
+    return (
+        <div>
+            this is Courses pages!
+        </div>
+    );
+};
+
+export default CoursesPage;
