73096db1fa93eaf85f44c2ea52fa10eacd9f9ffc
John Doe <hamedhasan422421@gmail.com>
2023-10-22 21:11:01 +0300

    🌟 Fix banner image size issue.... 🚀 Add new banner content and styles..... 🎉 Update button styles and alignment..... ✨ Refactor banner component code.... 🔧 Adjust background image position and size..... 📚 Update banner text and button labels.....

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
+import Banner from "@/components/HomePage/Banner";
 import Header from "@/components/HomePage/Header";
 
 export default function Home() {
   return (
    <>
-<main className="text-gray-600 body-font bg-[#1B143F] h-fit px-20">
+<main className="text-gray-600 body-font bg-[#1B143F] h-max px-20">
     <Header/>
- 
+   <Banner/>
+
+   {/* <p>lorem500</p> */}
 </main>
    </>
   )
---
+++ src/components/HomePage/Banner.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <section className="text-gray-600 body-font banner-images">
+  <div className="container mx-auto flex px-5 pb-12 pt-8 items-center justify-center flex-col">
+    
+    <div className="text-center lg:w-2/3 w-full">
+      <h1 className="title-font sm:text-4xl text-6xl mb-4 font-bold text-gray-200">Good coaching is good teaching</h1>
+      <p className="mb-8 px-14 py-3 text-gray-300 font-light leading-relaxed">In a coaching role, you ask the questions and rely more on your staff, who become the experts, to provide the information.</p>
+      <div className="flex justify-center gap-7">
+        <button className="banner_btn_left inline-flex text-white bg-transparent border-0 py-2 px-6 focus:outline-none  rounded text-lg">Join as Student</button>
+        <button className="ml-4 inline-flex text-white  bg-transparent border-0 py-2 px-6 focus:outline-none rounded text-lg banner_btn_right">Join as Instructor</button>
+      </div>
+    </div>
+  </div>
+ 
+</section>
+    );
+};
+
+export default Banner;
