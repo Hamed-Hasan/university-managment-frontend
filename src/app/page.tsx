bdc5698ecca72c67004748d3fe9f991a9b31291d
John Doe <hamedhasan422421@gmail.com>
2023-10-22 15:34:15 +0300

    🚀 Refactor code for better performance and readability. 🎨 Update styling in the Header component. 🔧 Fix a bug in the navigation menu. ✨ Add new features to the Header component. 🩹 Improve accessibility in the Header component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
-import { redirect } from "next/navigation";
+import Header from "@/components/HomePage/Header";
 
-const HomePage = () => {
-  return redirect("/profile");
-};
-
-export default HomePage;
+export default function Home() {
+  return (
+   <>
+<main className="text-gray-600 body-font bg-[#1B143F] h-fit px-20">
+    <Header/>
+ 
+</main>
+   </>
+  )
+}
