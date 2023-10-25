7f9058352a5118acfbce400a6b292531b41d582c
John Doe <hamedhasan422421@gmail.com>
2023-10-25 20:51:31 +0300

    🚀 Refactor the footer component 💡 Improve code readability 🐞 Fix a minor styling issue ✨ Add social media links to the footer 📝 Update documentation and comments 🎨 Enhance the footer design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Companies from "@/components/HomePage/Companies";
 import CourseNews from "@/components/HomePage/CourseNews";
 import Courses from "@/components/HomePage/Courses";
 import Dominate from "@/components/HomePage/Dominate";
+import HomeFooter from "@/components/HomePage/Footer";
 import Header from "@/components/HomePage/Header";
 import StudentSay from "@/components/HomePage/StudentSay";
 
@@ -19,6 +20,7 @@ export default function Home() {
         <StudentSay/>
         <CourseNews/>
         <Dominate/>
+        <HomeFooter/>
       </main>
     </>
   );
