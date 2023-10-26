d8547704dfb90c23cf87c1957d5206f556bb1ba7
John Doe <hamedhasan422421@gmail.com>
2023-10-26 14:57:53 +0300

    Refactor code for better performance 🛠️ Update Home page layout 🏡 Fix loading spinner display issue 🔄 Improve code structure and organization 🏗️ Update component order for better user experience 🔄📦

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,47 @@
+"use client"
+import React, { useState, useEffect } from "react";
+import Header from "@/components/HomePage/Header";
+import HomeFooter from "@/components/HomePage/Footer";
 import Banner from "@/components/HomePage/Banner";
 import Categories from "@/components/HomePage/Categories";
 import Companies from "@/components/HomePage/Companies";
 import CourseNews from "@/components/HomePage/CourseNews";
 import Courses from "@/components/HomePage/Courses";
 import Dominate from "@/components/HomePage/Dominate";
-import HomeFooter from "@/components/HomePage/Footer";
-import Header from "@/components/HomePage/Header";
 import StudentSay from "@/components/HomePage/StudentSay";
+import Loading from "./loading";
 
-export default function Home() {
+const components = [
+  Header,
+  Banner,
+  Companies,
+  Courses,
+  Categories,
+  StudentSay,
+  CourseNews,
+  Dominate,
+  HomeFooter,
+];
+
+function Home() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate a loading delay, e.g., fetching data from an API.
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
   return (
-    <>
-      <main className="text-gray-600 body-font bg-[#1B143F] h-max ">
-        <Header />
-        <Banner />
-        <Companies />
-        <Courses />
-        <Categories />
-        <StudentSay/>
-        <CourseNews/>
-        <Dominate/>
-        <HomeFooter/>
-      </main>
-    </>
+    <main className="text-gray-600 body-font bg-[#1B143F] h-max">
+      {loading ? (
+        <Loading />
+      ) : (
+        components.map((Component, index) => <Component key={index} />)
+      )}
+    </main>
   );
 }
+
+export default Home;
