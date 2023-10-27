32dc1f6f03821d97593503f40218c2c9187143a2
John Doe <hamedhasan422421@gmail.com>
2023-10-27 22:38:43 +0300

    ⚛️ Implemented responsive Area Chart component. ✨ Added data visualization with Recharts. 📊 Updated chart data for better accuracy. 🎨 Improved code structure and styling. 🔄 Refactored code to enhance performance. 📈 Enhanced the Area Chart with gradients. ⬇️ Decreased chart margin for a sleek look.

--- src/app/(withlayout)/profile/page.tsx
+++ src/app/(withlayout)/profile/page.tsx
@@ -1,7 +1,11 @@
+import Charts from "@/components/Chart/Charts";
+
 const ProfilePage = () => {
   return (
     <div>
-      <h1>Welcome back to your profile</h1>
+      {/* <h1>Welcome back to your profile</h1>
+       */}
+       <Charts/>
     </div>
   );
 };
