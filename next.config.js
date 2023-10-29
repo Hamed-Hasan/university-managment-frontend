d83540dcc95cd118f4d8a3958fd7f1bffb3a4606
John Doe <hamedhasan422421@gmail.com>
2023-10-30 00:32:13 +0300

    🌟 Add missing frontend components

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['images.unsplash.com', 'i.ibb.co'],
-  },
-};
+const nextConfig = {}
 
-module.exports = nextConfig;
+module.exports = nextConfig
