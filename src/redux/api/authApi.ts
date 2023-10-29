1c8c4b1bd4408bbfabb4bfe9d498e80163919765
John Doe <hamedhasan422421@gmail.com>
2023-10-29 23:53:04 +0300

    🚀 Added resetPassword and forgotPassword mutations 🐛 Fixed issues in resetPassword and forgotPassword mutations 💡 Improved error handling in resetPassword and forgotPassword mutations

--- src/redux/api/authApi.ts
+++ src/redux/api/authApi.ts
@@ -12,10 +12,25 @@ export const authApi = baseApi.injectEndpoints({
       }),
       invalidatesTags: [tagTypes.user],
     }),
-  
+    resetPassword: build.mutation({
+      query: (resetPasswordPayload) => ({
+        url: `${AUTH_URL}/reset-password`,
+        method: "POST",
+        data: resetPasswordPayload,
+      }),
+    }),
+    forgotPassword: build.mutation({
+      query: (forgotPasswordPayload) => ({
+        url: `${AUTH_URL}/forgot-password`,
+        method: "POST",
+        data: forgotPasswordPayload,
+      }),
+    }),
   }),
 });
 
 export const {
-  useUserLoginMutation
+  useUserLoginMutation,
+  useForgotPasswordMutation,
+  useResetPasswordMutation,
 } = authApi;
